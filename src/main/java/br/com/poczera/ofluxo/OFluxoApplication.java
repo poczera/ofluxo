fff191bea0a515cec8f0180027555feec1ac957c
Fernando <nandu.moraes@gmail.com>
2018-05-20 15:20:39 -0300

    first commit

---
+++ src/main/java/br/com/poczera/ofluxo/OFluxoApplication.java
@@ -0,0 +1,12 @@
+package br.com.poczera.ofluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OFluxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OFluxoApplication.class, args);
+	}
+}
